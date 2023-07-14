af9224b90a597fbffe2ee14ef907248e5be84b5e
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-15 00:19:08 +0300

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -4,6 +4,9 @@ import { nanoid } from "nanoid";
 
 const contactsPath = path.resolve("models", "contacts.json");
 
+const updateContacts = contacts =>
+	fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
 const listContacts = async () => {
 	const data = await fs.readFile(contactsPath);
 	return JSON.parse(data);
@@ -15,8 +18,6 @@ const getContactById = async contactId => {
 	return result || null;
 };
 
-const removeContact = async contactId => {};
-
 const addContact = async ({ name, email, phone }) => {
 	const contacts = await listContacts();
 	const newContact = {
@@ -25,18 +26,37 @@ const addContact = async ({ name, email, phone }) => {
 		email,
 		phone,
 	};
-
 	contacts.push(newContact);
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+	await updateContacts(contacts);
 	return newContact;
 };
 
-const updateContact = async (contactId, body) => {};
+const updateContactById = async (id, { name, email, phone }) => {
+	const contacts = await listContacts();
+	const index = contacts.findIndex(item => item.id === id);
+	if (index === -1) {
+		return null;
+	}
+	contacts[index] = { id, name, email, phone };
+	await updateContacts(contacts);
+	return contacts[index];
+};
+
+const removeContact = async contactId => {
+	const contacts = await listContacts();
+	const index = contacts.findIndex(item => item.id === contactId);
+	if (index === -1) {
+		return null;
+	}
+	const [result] = contacts.splice(index, 1);
+	await updateContacts(contacts);
+	return result || null;
+};
 
 export default {
 	listContacts,
 	getContactById,
 	removeContact,
 	addContact,
-	updateContact,
+	updateContactById,
 };
