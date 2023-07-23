dcd9244d621a711abdca420650aaa2e1646d91b1
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-23 14:31:29 +0300

    update console.log and process.exit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+
 import dotenv from "dotenv";
 
 import app from "./app.js";
@@ -10,8 +11,12 @@ const { DB_HOST, PORT } = process.env;
 mongoose
 	.connect(DB_HOST)
 	.then(() => {
+		console.log("Database connection successful");
 		app.listen(PORT, () => {
-			console.log(`Server running. Use our API on port: ${PORT}`);
+			console.log(`Server running. Use API on port: ${PORT}`);
 		});
 	})
-	.catch(error => console.log(error.message));
+	.catch(error => {
+		console.log(error.message);
+		process.exit(1);
+	});
