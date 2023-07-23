e759cbcb0ea57ac5a216fa2a2a739f6bca4db11c
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-23 14:29:01 +0300

    Update hooks.js

--- models/hooks.js
+++ models/hooks.js
@@ -2,3 +2,8 @@ export const handleSaveError = (error, data, next) => {
 	error.status = 400;
 	next();
 };
+
+export const handleUpdateValidate = function (next) {
+	this.options.runValidators = true;
+	next();
+};
