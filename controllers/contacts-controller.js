2bf55ec96447bc96653a844a337a4f238f06bd8e
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-23 14:26:44 +0300

    Update contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -21,28 +21,42 @@ const add = async (req, res) => {
 	res.status(201).json(result);
 };
 
-// const updateById = async (req, res) => {
-// 	const { contactId } = req.params;
-// 	const result = await contactsService.updateContactById(contactId, req.body);
-// 	if (!result) {
-// 		throw HttpError(404, "Not Found");
-// 	}
-// 	res.json(result);
-// };
+const updateById = async (req, res) => {
+	const { contactId } = req.params;
+	const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+		new: true,
+	});
+	if (!result) {
+		throw HttpError(404, "Not Found");
+	}
+	res.json(result);
+};
 
-// const deleteById = async (req, res) => {
-// 	const { contactId } = req.params;
-// 	const result = await contactsService.removeContact(contactId);
-// 	if (!result) {
-// 		throw HttpError(404, "Not Found");
-// 	}
-// 	res.status(200).json({ message: "Contact Deleted" });
-// };
+const updateStatusContact = async (req, res) => {
+	const { contactId } = req.params;
+	const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+		new: true,
+	});
+	if (!result) {
+		throw HttpError(404, "Not Found");
+	}
+	res.json(result);
+};
+
+const deleteById = async (req, res) => {
+	const { contactId } = req.params;
+	const result = await Contact.findByIdAndDelete(contactId);
+	if (!result) {
+		throw HttpError(404, "Not Found");
+	}
+	res.status(200).json({ message: "Contact Deleted" });
+};
 
 export default {
 	getAll: ctrlWrapper(getAll),
 	getById: ctrlWrapper(getById),
 	add: ctrlWrapper(add),
-	// updateById: ctrlWrapper(updateById),
-	// deleteById: ctrlWrapper(deleteById),
+	updateById: ctrlWrapper(updateById),
+	updateStatusContact: ctrlWrapper(updateStatusContact),
+	deleteById: ctrlWrapper(deleteById),
 };
