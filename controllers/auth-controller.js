7eaa31d3b039234400e5a39461efa5d4ca076691
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-29 23:21:25 +0300

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,7 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
 import User from "../models/user.js";
 
 import { HttpError } from "../helpers/index.js";
@@ -5,7 +9,14 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const register = async (req, res) => {
-	const newUser = await User.create(req.body);
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (user) {
+		throw HttpError(409, "Email in use");
+	}
+	const hashPassword = await bcrypt.hash(password, 10);
+	const newUser = await User.create({ ...req.body, password: hashPassword });
+
 	res.status(201).json({
 		user: {
 			email: newUser.email,
@@ -14,6 +25,35 @@ const register = async (req, res) => {
 	});
 };
 
+const { JWT_SECRET } = process.env;
+
+const login = async (req, res) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw HttpError(401, "Email or password is incorrect");
+	}
+	const passwordCompare = await bcrypt.compare(password, user.password);
+	if (!passwordCompare) {
+		throw HttpError(401, "Email or password is incorrect");
+	}
+
+	const payload = {
+		id: user._id,
+	};
+
+	const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+
+	res.status(201).json({
+		token,
+		user: {
+			email: user.email,
+			subscription: user.subscription,
+		},
+	});
+};
+
 export default {
 	register: ctrlWrapper(register),
+	login: ctrlWrapper(login),
 };
