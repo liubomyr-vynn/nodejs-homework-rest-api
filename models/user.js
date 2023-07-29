a94637a99f74268ca2ffa210c7f6127f71ec01e2
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-29 12:36:57 +0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+
+import { handleSaveError, handleUpdateValidate } from "./hooks.js";
+
+import { emailRegexp } from "../constants/user-constants.js";
+
+const userSchema = new Schema({
+	password: {
+		type: String,
+		minlength: 6,
+		required: [true, "Set password for user"],
+	},
+	email: {
+		type: String,
+		match: emailRegexp,
+		required: [true, "Email is required"],
+		unique: true,
+	},
+	subscription: {
+		type: String,
+		enum: ["starter", "pro", "business"],
+		default: "starter",
+	},
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: "user",
+	},
+	token: String,
+});
+
+userSchema.pre("findOneAndUpdate", handleUpdateValidate);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const User = model("user", userSchema);
+
+export default User;
