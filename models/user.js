ac026bf009f4c0e27c3297deb40dcb62a1804cef
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-29 23:22:09 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,29 +4,33 @@ import { handleSaveError, handleUpdateValidate } from "./hooks.js";
 
 import { emailRegexp } from "../constants/user-constants.js";
 
-const userSchema = new Schema({
-	password: {
-		type: String,
-		minlength: 6,
-		required: [true, "Set password for user"],
+const userSchema = new Schema(
+	{
+		password: {
+			type: String,
+			minlength: 6,
+			required: [true, "Set password for user"],
+		},
+		email: {
+			type: String,
+			match: emailRegexp,
+			unique: true,
+			required: [true, "Email is required"],
+			unique: true,
+		},
+		subscription: {
+			type: String,
+			enum: ["starter", "pro", "business"],
+			default: "starter",
+		},
+		owner: {
+			type: Schema.Types.ObjectId,
+			ref: "user",
+		},
+		token: String,
 	},
-	email: {
-		type: String,
-		match: emailRegexp,
-		required: [true, "Email is required"],
-		unique: true,
-	},
-	subscription: {
-		type: String,
-		enum: ["starter", "pro", "business"],
-		default: "starter",
-	},
-	owner: {
-		type: Schema.Types.ObjectId,
-		ref: "user",
-	},
-	token: String,
-});
+	{ versionKey: false },
+);
 
 userSchema.pre("findOneAndUpdate", handleUpdateValidate);
 
