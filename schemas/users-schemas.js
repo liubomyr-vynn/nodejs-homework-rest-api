9d54fdc3b4ba1492bca74a33388ad55a731a99bd
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-29 23:24:06 +0300

    add userLoginSchema

--- schemas/users-schemas.js
+++ schemas/users-schemas.js
@@ -13,6 +13,18 @@ const userRegisterSchema = Joi.object({
 	}),
 });
 
+const userLoginSchema = Joi.object({
+	email: Joi.string().pattern(emailRegexp).required().messages({
+		"string.empty": `Missing field 'email'`,
+		"any.required": `Missing required 'email' field`,
+	}),
+	password: Joi.string().min(6).required().messages({
+		"string.empty": `Missing field 'password'`,
+		"any.required": `Missing required 'password' field`,
+	}),
+});
+
 export default {
 	userRegisterSchema,
+	userLoginSchema,
 };
