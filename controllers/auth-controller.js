a00b9e097947aae0c1e4b8dda1b6d901e5e6afd9
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-31 00:17:14 +0300

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -8,6 +8,8 @@ import { HttpError } from "../helpers/index.js";
 
 import { ctrlWrapper } from "../decorators/index.js";
 
+const { JWT_SECRET } = process.env;
+
 const register = async (req, res) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
@@ -25,8 +27,6 @@ const register = async (req, res) => {
 	});
 };
 
-const { JWT_SECRET } = process.env;
-
 const login = async (req, res) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
@@ -43,8 +43,9 @@ const login = async (req, res) => {
 	};
 
 	const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+	await User.findByIdAndUpdate(user._id, { token });
 
-	res.status(201).json({
+	res.status(200).json({
 		token,
 		user: {
 			email: user.email,
@@ -53,7 +54,24 @@ const login = async (req, res) => {
 	});
 };
 
+const getCurrent = (req, res) => {
+	const { email, subscription } = req.user;
+	res.json({
+		email,
+		subscription,
+	});
+};
+
+const logout = async (req, res) => {
+	const { _id } = req.user;
+	await User.findByIdAndUpdate(_id, { token: "" });
+
+	res.status(204).json();
+};
+
 export default {
 	register: ctrlWrapper(register),
 	login: ctrlWrapper(login),
+	getCurrent: ctrlWrapper(getCurrent),
+	logout: ctrlWrapper(logout),
 };
